d739c30f5b94ab0ff134129337518d475973e926
Cazza73 <carolinew811@gmail.com>
2022-05-02 07:22:25 +0100

    fixed error in date js

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let hours = now.getHours();
 let minutes = now.getMinutes();
 
 if (minutes < 10) {
-  minutes = `0$(minutes)`;
+  minutes = `0${minutes}`;
 }
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
